90783cf1f8239353755aef492f29b06267bd97bd
Mark Gravestock <mark.gravestock@gmail.com>
2015-06-28 20:30:40 +0100

    New implementation based on objectmentor presentation

--- src/main/java/com/markgravestock/bowling_game/Game.java
+++ src/main/java/com/markgravestock/bowling_game/Game.java
@@ -1,20 +1,61 @@
 package com.markgravestock.bowling_game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
-	private int score;
+	private static final int SPARE_SCORE = 10;
+	private static final int STRIKE_SCORE = 10;
+
+	private List<Integer> pins = new ArrayList<Integer>();
 
 	public int getScore() {
+		int score = 0;
+		int index = 0;
+
+		for (int frame = 0; frame < 10; frame++) {
+
+			int frameScore = calculateFrameScore(index);
+
+			score += frameScore;
+
+			if (isStrike(pins.get(index))) {
+				score += calculateStrikeBonus(index);
+				index = index + 1;
+			} else if (isSpare(frameScore)) {
+				score += calculateSpareBonus(index);
+				index = index + 2;
+			}
+			else {
+				index = index + 2;				
+			}
+		}
+
 		return score;
 	}
 
-	public void bowl(int numberOfPinsHit) {
-		this.score += numberOfPinsHit;
-		
-		if (score == 15) {
-			score = 20;
-		}
-			
+	private Integer calculateSpareBonus(int index) {
+		return pins.get(index + 2);
 	}
 
+	private int calculateStrikeBonus(int index) {
+		return pins.get(index + 2) + pins.get(index + 3);
+	}
+
+	private int calculateFrameScore(int index) {
+		return pins.get(index) + pins.get(index + 1);
+	}
+
+	private boolean isStrike(int firstBowlInFrameScore) {
+		return firstBowlInFrameScore == STRIKE_SCORE;
+	}
+
+	private boolean isSpare(int frameScore) {
+		return frameScore == SPARE_SCORE;
+	}
+
+	public void bowl(int numberOfPinsHit) {
+		pins.add(numberOfPinsHit);
+	}
 }
--- src/test/java/com/markgravestock/bowling_game/test/ScoringTests.java
+++ src/test/java/com/markgravestock/bowling_game/test/ScoringTests.java
@@ -1,43 +1,66 @@
 package com.markgravestock.bowling_game.test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static java.util.stream.IntStream.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.markgravestock.bowling_game.Game;
 
-import static org.hamcrest.CoreMatchers.*;
-
 public class ScoringTests {
 
 	@Test
-	public void bowlingOneBowlThatIsntAStrike() {
+	public void playGutterGame() {
 		Game game = new Game();
-		game.bowl(1);
-		assertThat(game.getScore(), is(equalTo(1)));
+		
+		range(0, 20).forEach(num -> game.bowl(0));
+		
+		assertThat(game.getScore(), is(equalTo(0)));
 	}
 	
 	@Test
-	public void bowlingTwoScoresTwo() throws Exception {
+	public void rollAllOnes() {
 		Game game = new Game();
-		game.bowl(2);
-		assertThat(game.getScore(), is(equalTo(2)));
-	}
-
-	@Test
-	public void bowlingTwoBowlsThatArentASpare() throws Exception {
-		Game game = new Game();
-		game.bowl(1);
-		game.bowl(4);
-		assertThat(game.getScore(), is(equalTo(5)));
+		
+		range(0, 20).forEach(num -> game.bowl(1));
+		
+		assertThat(game.getScore(), is(equalTo(20)));		
 	}
 	
 	@Test
-	public void bowlingASpare() throws Exception {
+	public void rollOneSpare() {
 		Game game = new Game();
-		game.bowl(6);
-		game.bowl(4);
+		
 		game.bowl(5);
-		assertThat(game.getScore(), is(equalTo(20)));
+		game.bowl(5);
+		game.bowl(3);
+		range(3, 20).forEach(num -> game.bowl(0));
+		
+		assertThat(game.getScore(), is(equalTo(16)));		
 	}
+	
+	@Test
+	public void rollOneStrike() {
+		Game game = new Game();
+		
+		game.bowl(10);
+		game.bowl(3);
+		game.bowl(4);
+		range(4, 20).forEach(num -> game.bowl(0));
+		
+		assertThat(game.getScore(), is(equalTo(24)));		
+	}
+	
+	@Test
+	public void playPerfectGame() {
+		Game game = new Game();
+		
+		range(0, 12).forEach(num -> game.bowl(10));
+		
+		assertThat(game.getScore(), is(equalTo(300)));
+	}
+	
 }
